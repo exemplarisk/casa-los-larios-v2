bc2f0f1d50da1309f2b93721601ef612cd8467f9
Jonatan Schultz <jonatan.schultz@blaklader.com>
2023-11-08 18:41:07 +0100

    global styling added

--- casa-los-larios-v2/components/Layout/Layout.tsx
+++ casa-los-larios-v2/components/Layout/Layout.tsx
@@ -1,18 +1,24 @@
 import React, { ReactNode } from "react";
 import Header from "../Header/Header";
 import Footer from "../Footer/Footer";
-import styles from "../../styles/Home.module.css";
+import styles from "./Layout.module.css";
 
 type LayoutProps = {
   children: ReactNode;
 };
 
-const Layout: React.FC<LayoutProps> = ({ children }) => {
+const Layout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
   return (
     <div className={styles.container}>
-      <Header />
+      <header className={styles.header}>
+        <Header />
+      </header>
+
       <main className={styles.main}>{children}</main>
-      <Footer />
+
+      <footer className={styles.footer}>
+        <Footer />
+      </footer>
     </div>
   );
 };
