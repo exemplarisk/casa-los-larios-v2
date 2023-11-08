42f6fa5ab7f654742546c84f870365fcc2f08d0d
Jonatan Schultz <jonatan.schultz@blaklader.com>
2023-11-08 19:33:21 +0100

    global styles

--- casa-los-larios-v2/components/Layout/Layout.tsx
+++ casa-los-larios-v2/components/Layout/Layout.tsx
@@ -3,22 +3,12 @@ import Header from "../Header/Header";
 import Footer from "../Footer/Footer";
 import styles from "./Layout.module.css";
 
-type LayoutProps = {
-  children: ReactNode;
-};
-
 const Layout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
   return (
     <div className={styles.container}>
-      <header className={styles.header}>
-        <Header />
-      </header>
-
+      <Header />
       <main className={styles.main}>{children}</main>
-
-      <footer className={styles.footer}>
-        <Footer />
-      </footer>
+      <Footer />
     </div>
   );
 };
