c8ba7740bc2b1a4efb4f095a74d6b0ae87c1f65b
Jonatan Schultz <jonatan.schultz@blaklader.com>
2023-11-11 20:43:08 +0100

    new booking section

--- casa-los-larios-v2/components/BookingSection/BookingSection.tsx
+++ casa-los-larios-v2/components/BookingSection/BookingSection.tsx
@@ -1,12 +1,12 @@
-import React from "react";
-import styles from "./BookingSection.module.css";
-import ReactDatePicker from "react-datepicker";
+import React, { useState } from "react";
+import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
-import Swal from "sweetalert2";
+import styles from "./BookingSection.module.css";
 
 const BookingSection: React.FC = () => {
-  const [startDate, setStartDate] = React.useState(new Date());
-  const [endDate, setEndDate] = React.useState(new Date());
+  const [startDate, setStartDate] = useState<Date | null>(null);
+  const [endDate, setEndDate] = useState<Date | null>(null);
+  const pricePerNight = 150;
 
   const bookedDates = [
     new Date("2023-12-25"),
@@ -16,99 +16,81 @@ const BookingSection: React.FC = () => {
     new Date("2023-11-20"),
     new Date("2023-11-29"),
     new Date("2023-11-30"),
+    new Date("2024-01-08"),
+    new Date("2024-01-09"),
+    new Date("2024-01-10"),
+    new Date("2024-01-11"),
+    new Date("2024-01-12"),
   ];
 
-  const getTotalNights = (start, end) => {
-    const diffInTime = end.getTime() - start.getTime();
-    return diffInTime / (1000 * 3600 * 24); // Difference in days
-  };
-
-  const highlightedDates = {
-    "highlighted-dates": bookedDates,
-  };
-
-  const checkAvailability = () => {
-    let isUnavailable = bookedDates.some(
-      (bookedDate) => startDate <= bookedDate && bookedDate <= endDate
+  const isDateBlocked = (date: Date) => {
+    return bookedDates.some(
+      (bookedDate) => date.getTime() === bookedDate.getTime()
     );
-
-    if (isUnavailable) {
-      Swal.fire({
-        title: "Unavailable Dates Selected",
-        text: "Unfortunately, one or more of the chosen dates are not available. Please try different dates.",
-        icon: "error",
-        confirmButtonText: "OK",
-        confirmButtonColor: "#008f68",
-      });
-    } else {
-      Swal.fire({
-        title: "Dates Available!",
-        text: "The chosen dates are available. Please contact us below for booking.",
-        icon: "success",
-        confirmButtonText: "SURE THING",
-        confirmButtonColor: "#008f68",
-      }).then((result) => {
-        if (result.isConfirmed) {
-          document
-            .getElementById("contactSection")
-            ?.scrollIntoView({ behavior: "smooth" });
-        }
-      });
-    }
   };
+
+  const calculateTotalNights = () => {
+    if (!startDate || !endDate) return 0;
+    return (endDate.getTime() - startDate.getTime()) / (1000 * 3600 * 24);
+  };
+
+  const calculateTotalCost = () => {
+    return calculateTotalNights() * pricePerNight;
+  };
+
   return (
-    <section className={styles.bookingSection}>
-      <div className={styles.bookingContent}>
-        <div className={styles.bookingInfo}>
-          <h2>Book Your Stay</h2>
+    <div className={styles.container}>
+      <div className={styles.datePickerContainer}>
+        <DatePicker
+          selected={startDate}
+          onChange={(date) => setStartDate(date)}
+          startDate={startDate}
+          endDate={endDate}
+          selectsStart
+          minDate={new Date()}
+          excludeDates={bookedDates}
+          inline
+          monthsShown={1}
+        />
+        <DatePicker
+          selected={endDate}
+          onChange={(date) => setEndDate(date)}
+          selectsEnd
+          startDate={startDate}
+          endDate={endDate}
+          minDate={startDate}
+          excludeDates={bookedDates}
+          inline
+          monthsShown={1}
+        />
+      </div>
+      <div className={styles.summaryContainer}>
+        <div className={styles.summaryHeader}>
           <p>
-            Discover the perfect getaway at Casa Los Larios. Select your
-            preferred dates to see availability.
+            <strong>Total Nights:</strong> <span>{calculateTotalNights()}</span>
+          </p>
+          <p>
+            <strong>Total Cost:</strong>{" "}
+            <span>SEK {calculateTotalCost().toFixed(2)}</span>
           </p>
         </div>
-        <div className={styles.bookingForm}>
-          <div className={styles.datePicker}>
-            <p>Check-in:</p>
-            <ReactDatePicker
-              selected={startDate}
-              onChange={(date: Date) => setStartDate(date)}
-              selectsStart
-              startDate={startDate}
-              endDate={endDate}
-              excludeDates={bookedDates}
-              minDate={new Date()}
-              dateFormat="dd/MM/yyyy"
-              highlightDates={highlightedDates}
-            />
-          </div>
-          <div className={styles.datePicker}>
-            <p>Check-out:</p>
-            <ReactDatePicker
-              selected={endDate}
-              onChange={(date: Date) => setEndDate(date)}
-              selectsEnd
-              startDate={startDate}
-              endDate={endDate}
-              excludeDates={bookedDates}
-              minDate={startDate}
-              dateFormat="dd/MM/yyyy"
-              highlightDates={highlightedDates}
-            />
-          </div>
-          {startDate && endDate && (
-            <div className={styles.bookingSummary}>
-              <h3>Your Stay:</h3>
-              <p>Check-in: {startDate.toLocaleDateString()}</p>
-              <p>Check-out: {endDate.toLocaleDateString()}</p>
-              <p>Total Nights: {getTotalNights(startDate, endDate)}</p>
-            </div>
-          )}
-          <button onClick={checkAvailability} className={styles.bookButton}>
-            Check Availability
-          </button>
+        <div className={styles.dateSelection}>
+          <p>
+            <strong>Check-in:</strong>{" "}
+            <span>
+              {startDate ? startDate.toLocaleDateString() : "Select date"}
+            </span>
+          </p>
+          <p>
+            <strong>Check-out:</strong>{" "}
+            <span>
+              {endDate ? endDate.toLocaleDateString() : "Select date"}
+            </span>
+          </p>
         </div>
+        <button className={styles.reserveButton}>Reserve</button>
       </div>
-    </section>
+    </div>
   );
 };
 
