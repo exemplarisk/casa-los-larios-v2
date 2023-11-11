ae4878b3622adbb0b2180318311f33576b6e17db
Jonatan Schultz <jonatan.schultz@blaklader.com>
2023-11-11 15:42:10 +0100

    about

--- casa-los-larios-v2/components/ApartmentDetails/ApartmentDetails.tsx
+++ casa-los-larios-v2/components/ApartmentDetails/ApartmentDetails.tsx
@@ -1,74 +1,19 @@
-import React, { useState } from "react";
+import React from "react";
 import styles from "./ApartmentDetails.module.css";
+import apartmentInfo from "./apartmentInfoData";
 
 const ApartmentDetails = () => {
-  const images = [
-    "/familia1.jpg",
-    "/summergetaway.jpg",
-    "/summergetaway1.jpg",
-    "/seaview1.jpg",
-  ];
-
-  const [currentImageIndex, setCurrentImageIndex] = useState(0);
-
-  const nextImage = () => {
-    setCurrentImageIndex((prevIndex) =>
-      prevIndex === images.length - 1 ? 0 : prevIndex + 1
-    );
-  };
-
-  const prevImage = () => {
-    setCurrentImageIndex((prevIndex) =>
-      prevIndex === 0 ? images.length - 1 : prevIndex - 1
-    );
-  };
-
   return (
-    <section className={styles.apartmentDetails}>
-      <div className={styles.detailsContent}>
-        <div className={styles.detailsText}>
-          <h2>About Casa Los Larios</h2>
-          <p>
-            Located in Torrevieja, Spain, Casa Los Larios
-            offers an exquisite getaway with breathtaking sea views. Experience
-            the comfort and elegance of our well-appointed apartment.
-          </p>
-          <ul>
-            <li>Stunning vistas from a sun-kissed balcony</li>
-            <li>Two pristine bedrooms designed for relaxation</li>
-            <li>A culinary-ready kitchen with state-of-the-art appliances</li>
-            <li>
-              High-speed Wi-Fi and a large-screen TV for your entertainment
-            </li>
-            <li>An exclusive pool for serene aquatic retreats</li>
-            <li>Proximity to gourmet restaurants and local attractions</li>
-          </ul>
-          <button className={styles.reservationButton}>
-            Reserve Your Stay
-          </button>
-        </div>
-        <div className={styles.detailsImage}>
-          <div className={styles.imageSlider}>
-            <button className={styles.prevButton} onClick={prevImage}>
-              &#10094;
-            </button>
-            {images.map((image, index) => (
-              <img
-                key={index}
-                src={image}
-                alt={`Casa Los Larios View ${index + 1}`}
-                className={
-                  index === currentImageIndex
-                    ? styles.active
-                    : styles.sliderImage
-                }
-              />
-            ))}
-            <button className={styles.nextButton} onClick={nextImage}>
-              &#10095;
-            </button>
+    <section className={styles.infoSection}>
+      <h2 className={styles.infoHeading}>WHAT YOU NEED TO KNOW</h2>
+      <div className={styles.infoGrid}>
+        {apartmentInfo.map((info, index) => (
+          <div key={index} className={styles.infoItem}>
+            <info.icon className={styles.infoIcon} />
+            <h3 className={styles.infoTitle}>{info.title}</h3>
+            <p className={styles.infoDescription}>{info.description}</p>
           </div>
-        </div>
+        ))}
       </div>
     </section>
   );
---
+++ casa-los-larios-v2/components/ApartmentDetails/apartmentInfoData.ts
@@ -0,0 +1,48 @@
+import {
+  FaSwimmingPool,
+  FaPlane,
+  FaBed,
+  FaMapSigns,
+  FaParking,
+  FaPaw,
+} from "react-icons/fa";
+
+const apartmentInfo = [
+  {
+    icon: FaBed,
+    title: "ACCOMMODATION",
+    description:
+      "Spacious two-bedroom apartment with sea-view balcony and all modern amenities.",
+  },
+  {
+    icon: FaParking,
+    title: "PARKING",
+    description: "Complimentary secure parking with 24/7 surveillance.",
+  },
+  {
+    icon: FaMapSigns,
+    title: "DISTANCES",
+    description:
+      "5 minutes walk from the city center and 10 minutes drive to historical landmarks.",
+  },
+  {
+    icon: FaPlane,
+    title: "AIRPORTS",
+    description:
+      "Easily accessible, only 15 minutes away from the international airport.",
+  },
+  {
+    icon: FaPaw,
+    title: "PETS",
+    description:
+      "Pet-friendly with nearby parks and beaches for your furry friends.",
+  },
+  {
+    icon: FaSwimmingPool,
+    title: "SEASIDE",
+    description:
+      "Direct access to private beaches and a stunning infinity pool.",
+  },
+];
+
+export default apartmentInfo;
