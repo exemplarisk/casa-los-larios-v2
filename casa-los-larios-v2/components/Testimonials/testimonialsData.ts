77cfc5a7775581ffbdaf0353082bb99228eb2ea3
Jonatan Schultz <jonatan.schultz@blaklader.com>
2023-11-11 17:51:05 +0100

    Testimonials

--- casa-los-larios-v2/components/Testimonials/testimonialsData.ts
+++ casa-los-larios-v2/components/Testimonials/testimonialsData.ts
@@ -1,22 +1,22 @@
 const testimonials = [
   {
     quote:
-      "Vi rekommenderar varmt Casa Los Larios. Lägenheten är rymlig, fräsch, och allt man behöver finns på plats. Poolen är underbar och restaurangerna i närheten är riktigt grymma!",
+      "Vi rekommenderar varmt Casa Los Larios. Lägenheten är rymlig, fräsch, och allt man behöver finns på plats. Poolen är perfekt svalkande och vi kan gott rekommendera kockens specialité på Concierto!",
     author: "Jonatan & Wilma",
   },
   {
     quote:
-      "Orihuela Costa är underbart! Flera stränder på promenadavstånd från lägenheten med massor av trevliga restauranger på vägen!",
+      "Orihuela Costa är underbart! Flera stränder på promenadavstånd från lägenheten med massor av trevliga restauranger på vägen",
     author: "Sara med vänner",
   },
   {
     quote:
-      "Alltig lika trevligt att komma tillbaka till Casa Los Larios!",
+      "Alltid lika trevligt att komma tillbaka till Casa Los Larios",
     author: "Fam Simonsson",
   },
   {
     quote:
-      "Rymlig lägenhet med underbar terrass! Svalkande pool, strålande sol, och ett trevligt bemötande gjorde vår resa 5/5",
+      "Rymlig lägenhet med underbar terrass! Härlig pool, strålande sol, och ett trevligt bemötande gjorde vår resa tip top",
     author: "A Lundström",
   },
 ];
