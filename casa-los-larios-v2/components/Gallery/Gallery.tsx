e4f9cee8981494dcc0ea7869bca2d234ef9f11ab
Jonatan Schultz <jonatan.schultz@blaklader.com>
2023-11-11 19:10:48 +0100

    gallery clickable images

--- casa-los-larios-v2/components/Gallery/Gallery.tsx
+++ casa-los-larios-v2/components/Gallery/Gallery.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./Gallery.module.css";
 
 const Gallery: React.FC = () => {
@@ -7,14 +7,39 @@ const Gallery: React.FC = () => {
     "/summergetaway.jpg",
     "/summergetaway1.jpg",
     "/seaview1.jpg",
+    "/seaview2.jpg",
+    "/beachwalk1.jpg",
+    "/beachfront.jpg",
+    "/attraction1.jpg",
+    "/aparoutd.jpg",
+    "/summergetaway1.jpg",
+    "/seaview1.jpg",
+    "/familia1.jpg",
   ];
 
+  const [activeImage, setActiveImage] = useState<string | null>(null);
+
   return (
+    <>
+      {activeImage && (
+        <div className={styles.modal} onClick={() => setActiveImage(null)}>
+          <img
+            src={activeImage}
+            alt="Enlarged view"
+            className={styles.modalContent}
+          />
+          <span className={styles.close}>&times;</span>
+        </div>
+      )}
       <div className={styles.gallery}>
         <h2 className={styles.title}>Apartment Photo Gallery</h2>
         <div className={styles.grid}>
           {images.map((src, index) => (
-            <div key={index} className={styles.imageWrapper}>
+            <div
+              key={index}
+              className={styles.imageWrapper}
+              onClick={() => setActiveImage(src)}
+            >
               <img
                 src={src}
                 alt={`Gallery Image ${index + 1}`}
@@ -24,6 +49,7 @@ const Gallery: React.FC = () => {
           ))}
         </div>
       </div>
+    </>
   );
 };
 
