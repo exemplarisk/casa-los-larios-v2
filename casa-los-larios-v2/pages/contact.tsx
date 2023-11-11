0c9b8e03260ef358881fe19ea28b24ebd025d28c
Jonatan Schultz <jonatan.schultz@blaklader.com>
2023-11-12 00:56:00 +0100

    contact style

--- casa-los-larios-v2/pages/contact.tsx
+++ casa-los-larios-v2/pages/contact.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import ContactInfo from "../components/ContactInfo/ContactInfo";
 import Head from "next/head";
 import Layout from "../components/Layout/Layout";
+import styles from '../components/ContactInfo/ContactInfo.module.css'
 
 function contact() {
   return (
@@ -11,7 +12,11 @@ function contact() {
         <meta name="description" content="Contact Casa-Los-Larios" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <ContactInfo/>
+      <div className={styles.container}>
+        <div className="overlay">
+          <ContactInfo />
+        </div>
+      </div>
     </Layout>
   );
 }
