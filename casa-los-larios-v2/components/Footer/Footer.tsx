7c55ee08fdb4eaf733f50535ba0b94953cd094fb
Jonatan Schultz <jonatan.schultz@blaklader.com>
2023-11-11 14:23:39 +0100

    Footer content & styles

--- casa-los-larios-v2/components/Footer/Footer.tsx
+++ casa-los-larios-v2/components/Footer/Footer.tsx
@@ -8,6 +8,9 @@ import {
 } from "@fortawesome/free-brands-svg-icons";
 
 const Footer: React.FC = () => {
+  const mapSrc =
+    "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3146.548187631296!2d-0.7429988846775689!3d37.940984279729584!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xd6307ceef5fb59d%3A0x66c4257a68279c08!2sCalle%20Guapiles%2C%207%2C%2003189%20Orihuela%2C%20Alicante%2C%20Spanien!5e0!3m2!1ssv!2sse!4v1625486117153!5m2!1ssv!2sse";
+
   return (
     <footer className={styles.footer}>
       <div className={styles.footerContent}>
@@ -17,7 +20,7 @@ const Footer: React.FC = () => {
         </section>
         <address className={styles.contactInfo}>
           <h3>Contact Us</h3>
-          <p>Location: Torrevieja, Spain</p>
+          <p>Location: C. Guapiles 7, 03189, Orihuela, Alicante, Spain</p>
           <p>
             Email:{" "}
             <a href="mailto:info@casaloslarios.com">info@casaloslarios.com</a>
@@ -26,6 +29,15 @@ const Footer: React.FC = () => {
             Phone: <a href="tel:+123-456-7890">+123-456-7890</a>
           </p>
         </address>
+        <div className={styles.mapContainer}>
+          <iframe
+            className={styles.mapIframe}
+            src={mapSrc}
+            allowFullScreen={true}
+            loading="lazy"
+          ></iframe>
+        </div>
+
         <nav className={styles.socialLinks}>
           <h3>Connect</h3>
           <a
